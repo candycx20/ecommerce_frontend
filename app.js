cdc31402cddf0c61656dd7b72c6720a01dac52de
candycx20 <candycarrillo060@gmail.com>
2024-10-03 17:02:45 -0600

    Cors p2

--- app.js
+++ app.js
@@ -11,29 +11,13 @@ import DetallePedidoRoute from './routes/DetallePedidoRoute.js'
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-
-app.use((req, res, next) => {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "*"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Private-Network", true);
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", 7200);
+app.use(cors({
+    origin: '*',  // Permite todas las solicitudes de cualquier origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Permite los métodos necesarios
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Content-Type-Options', 'Accept', 'X-Requested-With', 'Origin']
+  }));
   
-    next();
-  });
+app.use(express.json())
 
 app.use('/productos', ProductoRoute);
 app.use('/carritoCompras', CarritoCompraRoute);
