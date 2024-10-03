812b9dc776ffcc87217d9908b7265c916a4c45ba
candycx20 <candycarrillo060@gmail.com>
2024-10-03 17:09:54 -0600

    Cors 4

--- app.js
+++ app.js
@@ -11,12 +11,26 @@ import DetallePedidoRoute from './routes/DetallePedidoRoute.js'
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://react-ecommerce-candy.s3-website-us-east-2.amazonaws.com',  // Solo permite este dominio
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,  // Permite cookies y credenciales
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "*"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+  });
   
   
 app.use(express.json())
