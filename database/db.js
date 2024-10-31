35c34f19870d37e0865c6df7f3be3c4088b0bea5
candycx20 <candycarrillo060@gmail.com>
2024-10-31 01:38:26 -0600

    Rutas 2

--- database/db.js
+++ database/db.js
@@ -4,13 +4,12 @@ const database = process.env.MYSQL_DATABASE;
 const username = process.env.MYSQL_USER;
 const password = process.env.MYSQL_PASSWORD;
 const host = process.env.MYSQL_HOST;
-const port = process.env.PORT;
 
 
 const db = new Sequelize(database, username, password,{
-    host: host,
+    host,
     dialect: 'mysql',
-    port: port
+    port: process.env.MYSQL_PORT
 })
 
 export default db
